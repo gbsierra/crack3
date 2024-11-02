ac07923d2af5310f91eda91fa7443702c7494b1d
gbsierra <gbatarseh@student.sierracollege.edu>
2024-11-02 10:49:17 -0700

    commiting required changes

--- crack.c
+++ crack.c
@@ -11,38 +11,54 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
-
-int main(int argc, char *argv[])
-{
-    if (argc < 3) 
-    {
+//Main Function
+int main(int argc, char *argv[]) { 
+    //check if correct amount of args
+    if (argc < 3) {
         printf("Usage: %s hash_file dictionary_file\n", argv[0]);
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
+    int amt = 0;
+    //read hashes into array
+    char **hashes = loadFileAA(argv[1], &size);
     
     // CHALLENGE1: Sort the hashes using qsort.
     
-    // TODO
     // Open the password file for reading.
+    FILE *pass_file = fopen(argv[2], "r");
 
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
+    char curr_hash[HASH_LEN];
+    //for each password
+    while(fgets(curr_hash, HASH_LEN, pass_file) != NULL){
+        for(int i=0; i<strlen(curr_hash); i++){
+            if(curr_hash[i] == '\n') curr_hash[i] = '\0';
+        }
+        //hash it
+        char *hash = md5(curr_hash, strlen(curr_hash));
+        //use the array search (function from fileutil.h)
+
+        //if found
+        char *found_hash = linearSearch(hash, hashes, size);
+        if(found_hash){
+            //display the password and hash
+            printf("%s %s\n", found_hash, curr_hash);
+            //keep track of amt
+            amt++;
+        }
+
+
+    }
+    
     // CHALLENGE1: Use binary search instead of linear search.
 
     // TODO
     // When done with the file:
     //   Close the file
+    fclose(pass_file);
     //   Display the number of hashes found.
+    printf("%d found\n", amt);
     //   Free up memory.
+    freeAA(hashes, size);
 }
---
+++ fileutil.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileutil.h"
+
+#define ALLOC_CAPACITY_START 10
+#define ALLOC_CAPACITY_INCREMENT 10
+#define MAX_LINE_LENGTH 1000
+
+
+// Return a pointer to an array of strings. (Also sets the value of *size to be the number of valid entries in the array (not the total array length)).
+//loadFileAA()
+char ** loadFileAA(char *filename, int *size) {
+
+	//inital Array of Array capacity
+	int capacity = ALLOC_CAPACITY_START;
+
+	//open file for read
+	FILE *file1 = fopen(filename, "r");
+	//check for error
+	if (!file1) {
+	    perror("Can't open file");
+	    exit(1);
+	}
+	
+	// TODO
+	// Allocate memory for an array of strings (arr).
+	char ** arrOfStrings = malloc(ALLOC_CAPACITY_START * sizeof(char *));
+
+	// set size to 0
+	*size = 0;
+
+	char currentLine[MAX_LINE_LENGTH];
+	// Read the file line by line.
+	while(fgets(currentLine, MAX_LINE_LENGTH, file1)){
+
+		// Trim newline.
+		char *nl = strchr(currentLine, '\n');
+		if(nl) {
+			*nl = '\0';
+		}
+
+		// Expand array if necessary (realloc).
+		if(*size == capacity){
+			capacity += ALLOC_CAPACITY_INCREMENT;
+			arrOfStrings = realloc(arrOfStrings, capacity * sizeof(char *));
+		}
+
+		//   Allocate memory for the string (str).
+		char * str = malloc(strlen(currentLine) + 1 * sizeof(char *));
+		//   Copy each line into the string (use strcpy).
+		strcpy(str, currentLine);
+		//   Attach the string to the large array (assignment =).
+		arrOfStrings[*size] = str;
+		*size = *size + 1;
+	}
+
+
+    // Close the file.
+	fclose(file1);
+	
+	// The size should be the number of entries in the array.
+	//*size = count;??
+	
+	// Return pointer to the array of strings.
+	return arrOfStrings;
+}
+
+
+// Search the array for the target string. (returns the string or NULL)
+//substringSearchAA()
+char * substringSearchAA(char *target, char **lines, int size) {
+	for(int i=0; i<size; i++){
+		if(strstr(lines[i], target)) return lines[i];
+	}
+	return NULL;
+}
+
+
+// Free the memory used by the array
+//freeAA()
+void freeAA(char ** arr, int size){
+	for(int i=0; i<size; i++){
+		free(arr[i]);
+	}
+	free(arr);
+}
+
+//linearSearch() use strcmp()
+char * linearSearch(char *target, char** lines, int size) {
+	for(int i=0; i<size; i++){
+		if(!strcmp(lines[i], target)) return lines[i];
+	}
+	return NULL;
+}
+
+
+
+
+
+char (*loadFile2D(char *filename, int *size))[COLS]
+{
+	FILE *in = fopen(filename, "r");
+	if (!in)
+	{
+	    perror("Can't open file");
+	    exit(1);
+	}
+	
+	// TODO
+	// Allocate memory for an 2D array, using COLS as the width.
+	// Read the file line by line into a buffer.
+    //   Trim newline.
+	//   Expand array if necessary (realloc).
+	//   Copy each line from the buffer into the array (use strcpy).
+    // Close the file.
+	
+	// The size should be the number of entries in the array.
+	*size = 0;
+	
+	// Return pointer to the array.
+	return NULL;
+}
+char * substringSearch2D(char *target, char (*lines)[COLS], int size)
+{
+    
+    return NULL;
+}
+void free2D(char (*arr)[COLS]) {
+}
